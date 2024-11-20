2d172f9a7bdcee2f2492763b74be83edba5f6e4e
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-11-20 11:27:37 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ const [currentPage, setCurrentPage] = useState('products');
    <nav className="navbar">
     <div className="container-fluid">
      <a className='navbar-brand' href="#">Bain's Fresh Market</a>
+     <ul className="navbar-nav ms-auto">
+      <li className="nav-item">
+       <button className="btn btn-link nav-link" onClick={()=> setCurrentPage('products')}>Products</button>
+      </li>
+     </ul>
     </div>
    </nav>
   </div>
