d4244558004cc12aa994d25d11a1d78f8847d76d
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-11-20 11:25:09 -0600

    update.cart.js

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,11 +1,41 @@
 import React from "react";
 
 const Cart =({cart, updateCart, checkout}) => {
- const subtotal =cart.reduce((total,item)) => total + item.price * item.quantity, 0);
+ const subtotal =cart.reduce((total,item) => total + item.price * item.quantity, 0);
 
  const handleRemove = (id) => {
   updateCart((prevCart) => prevCart.filter((item) => item.id !==id));
  };
 
- 
+ const handleAddONe = (id) =>{
+  updateCart((prevCart) => prevCart.map((item) => (item.id === id ? {...item, quantity: item.quantity + 1} : item))
+ );
+ };
+
+ const handleRemoveOne = (id) => {
+  updateCart((prevCart) => prevCart.map((item) => (item.id === id ? {...item, quantity: item.quantity - 1} : item)).filter((item) =>item.quantity > 0)
+ );
+ };
+
+ return(
+  <div className="cart">
+   <h2>Your CArt</h2>
+   {cart.length ===0 ? (
+    <p>Your cart is empty</p>
+   ): (
+    <table className="table">
+     <thead>
+      <tr>
+       <th>Item</th>
+       <th>Quantity</th>
+       <th>Price</th>
+       <th>Actions</th>
+      </tr>
+     </thead>
+    </table>
+   )}
+  </div>
+ )
+
+
 }
