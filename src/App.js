62d1ce6b964c4da58c01604d162fda62bb2f936d
Michael Bain <150988106+Mikebain2387@users.noreply.github.com>
2024-11-20 10:31:07 -0600

    app,js. index

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+
+function App(){
+ return(
+  <div className="container">
+   <nav className="navbar">
+    <div className="container-fluid">
+     <a className='navbar-brand' href="#">Bain's Fresh Market</a>
+    </div>
+   </nav>
+  </div>
+ )
+}
+
+
+
+
+export default App;
